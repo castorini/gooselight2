71028dfd61330ffd30d642f8203fe2df89c63d10
Edwin Zhang <edwinzhang64@gmail.com>
2020-03-27 22:50:36 -0400

    Update Blacklight for latest Anserini changes

--- covid/app/helpers/application_helper.rb
+++ covid/app/helpers/application_helper.rb
@@ -1,20 +1,2 @@
 module ApplicationHelper
-  def get_document_link(doc)
-    doi_url = " https://doi.org/"
-    pmc_url = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
-    pubmed_url = "https://www.ncbi.nlm.nih.gov/pubmed/"
-    s2_url = "https://www.semanticscholar.org/paper/"
-
-    if doc.has?("doi")
-      return doi_url + doc.fetch("doi").gsub("[", "").gsub("]", "").gsub("\"", "")
-    elsif doc.has?("pmcid")
-      return pmc_url + doc.fetch("pmcid")
-    elsif doc.has?("pubmed_id")
-      return pmc_url + doc.fetch("pubmed_id")
-    elsif doc.has?("sha")
-      return s2_url + doc.fetch("sha").split(";")[-1].strip()
-    else
-      return ""
-    end
-  end
 end
